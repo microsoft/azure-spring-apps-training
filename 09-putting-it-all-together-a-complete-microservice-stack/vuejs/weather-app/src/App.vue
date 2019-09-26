859d2a97e0f26182b4fca8a87d3f72087c97e0ef
Julien Dubois <julien.dubois@gmail.com>
2019-09-26 11:37:47 +0200

    Add simple VueJS application

---
+++ 09-putting-it-all-together-a-complete-microservice-stack/vuejs/weather-app/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-icon>mdi-cloud</v-icon>&nbsp;
+      <v-toolbar-title>What is the weather today?</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <ApiGateway/>
+            <hr/>
+            It is cloudy
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text"><a  class="white--text" href="https://github.com/jdubois/azure-spring-cloud-training">Azure Spring Cloud training</a></span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import ApiGateway from './components/ApiGateway'
+
+  export default {
+    components: {
+      ApiGateway
+    },
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
---
+++ 09-putting-it-all-together-a-complete-microservice-stack/vuejs/weather-app/src/components/ApiGateway.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container
+    id="input-usage"
+    fluid
+  >
+    <v-row>
+        <v-col cols="3">
+             <v-icon size="100">mdi-cloud</v-icon>
+        </v-col>
+        <v-col cols="6">
+            <v-form ref="form">
+                <v-text-field label="Spring Cloud Gateway URL" v-model="apigateway"/>
+                <v-btn class="mr-4" @click="getWeather">
+                    Go
+                </v-btn>
+            </v-form>
+        </v-col>
+        
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        apigateway: "https://azure-spring-cloud-training-gateway.azureapps.io"
+      }
+    },
+    methods: {
+      getWeather () {
+          this.data.apigateway = "boom";
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
