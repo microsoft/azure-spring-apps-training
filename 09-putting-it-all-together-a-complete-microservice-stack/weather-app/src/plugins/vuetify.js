64af36e5a516a81d13960f878fcb21e1a4baa133
Julien Dubois <julien.dubois@gmail.com>
2019-10-02 13:24:44 +0200

    Add Microsoft license headers

--- 09-putting-it-all-together-a-complete-microservice-stack/weather-app/src/plugins/vuetify.js
+++ 09-putting-it-all-together-a-complete-microservice-stack/weather-app/src/plugins/vuetify.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
